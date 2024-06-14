7f80297ea8cc1ae0d3472be997ba709335fc66b9
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-06-14 22:52:28 +0200

    project set up and homepage started

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src="./buena_logo.png" alt="buena logo" />
+      <p>Register with Buena and start booking apartments</p>
+      <button>Register here</button>
     </div>
   );
 }
